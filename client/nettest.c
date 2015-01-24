397c53065ab2775ddc2c753f37427673558f0a76
Eric Park <jeztek@users.noreply.github.com>
2015-01-24 02:02:39 -0800

    network testing

--- client/nettest.c
+++ client/nettest.c
@@ -1,13 +1,20 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <sys/socket.h>
+
 #include "network.h"
 #include "common.h"
 
-#define PORT 80
+#define PORT 8001
 
 int main(int argc, char* argv[])
 {
   network_t ctx;
+  char buf[4];
+  buf[0] = 0xde;
+  buf[1] = 0xad;
+  buf[2] = 0xbe;
+  buf[3] = 0xef;
 
   if (argc < 2) {
     printf("Usage: %s <hostname>\n", argv[0]);
@@ -20,6 +27,7 @@ int main(int argc, char* argv[])
   }
   printf("Connected to %s on port %d\n", argv[1], PORT);
 
+  send(ctx.fd, buf, 4, 0);
   close(ctx.fd);
   return 0;
 }
--- server/main.go
+++ server/main.go
@@ -1,5 +1,53 @@
 package main
 
+import (
+    "fmt"
+    "net"
+    "os"
+)
+
+const (
+    CONN_HOST = "localhost"
+    CONN_PORT = "8001"
+    CONN_TYPE = "tcp"
+
+	CONN_MAX_SIZE = 8191
+)
+
 func main() {
 
+    // Listen for incoming connections
+    l, err := net.Listen(CONN_TYPE, CONN_HOST + ":" + CONN_PORT)
+    if err != nil {
+        fmt.Printf("Error listening on %s:%s: %s", CONN_HOST, CONN_PORT,
+			err.Error())
+        os.Exit(1)
+    }
+
+    defer l.Close()
+
+    fmt.Println("Listening on " + CONN_HOST + ":" + CONN_PORT)
+    for {
+		// Accept connection
+        conn, err := l.Accept()
+        if err != nil {
+            fmt.Println("Error accepting connection: ", err.Error())
+            os.Exit(1)
+        }
+		// Launch goroutine to service connection
+        go handleRequest(conn)
+    }
+}
+
+func handleRequest(conn net.Conn) {
+	buf := make([]byte, CONN_MAX_SIZE)
+
+	readLen, err := conn.Read(buf)
+	if err != nil {
+		fmt.Println("Error reading from client:", err.Error())
+	}
+	fmt.Printf("Got %d bytes from client\n", readLen);
+	fmt.Printf("rx: % X\n", buf[:readLen])
+	//conn.Write([]byte("Message received"))
+	conn.Close()
 }

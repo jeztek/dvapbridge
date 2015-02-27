bf42350858a95894859de8d2fac5e2a8f0dea534
Eric Park <jeztek@users.noreply.github.com>
2015-02-27 09:12:52 -0800

    WIP: server

--- server/main.go
+++ server/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"bufio"
     "fmt"
+	"io"
     "net"
     "os"
 )
@@ -14,29 +16,126 @@ const (
 	CONN_MAX_SIZE = 8191
 )
 
-func main() {
+type Client struct {
+	connection *net.Conn
+	incoming chan []byte
+	outgoing chan []byte
+	reader   *bufio.Reader
+	writer   *bufio.Writer
+	clients  *map[string]*Client
+}
 
-    // Listen for incoming connections
-    l, err := net.Listen(CONN_TYPE, CONN_HOST + ":" + CONN_PORT)
+func (client *Client) Read() {
+	data := make([]byte, CONN_MAX_SIZE)
+	for {
+		n, err := client.reader.Read(data)
+		if (err != nil) {
+			if (err == io.EOF) {
+				fmt.Printf("Client disconnected\n")
+			}
+			fmt.Printf("Error reading from client\n")
+			continue
+		}
+		client.incoming <- data[:n]
+	}
+}
+
+func (client *Client) Write() {
+	for data := range client.outgoing {
+		_, err := client.writer.Write(data)
+		client.writer.Flush()
+		if (err != nil) {
+			fmt.Printf("Error writing to client\n")
+			continue
+		}
+	}
+}
+
+func (client *Client) Listen() {
+	go client.Read()
+	go client.Write()
+}
+
+func NewClient(connection net.Conn, clients *map[string]*Client) *Client {
+	client := &Client{
+		connection: &connection,
+		incoming: make(chan []byte, CONN_MAX_SIZE),
+		outgoing: make(chan []byte, CONN_MAX_SIZE),
+		reader:   bufio.NewReader(connection),
+		writer:   bufio.NewWriter(connection),
+		clients:  clients,
+	}
+	client.Listen()
+	return client
+}
+
+type Server struct {
+	clients  map[string]*Client
+	joins    chan net.Conn
+	incoming chan []byte
+	outgoing chan []byte
+}
+
+func (server *Server) Broadcast(data []byte) {
+	for _, client := range server.clients {
+		client.outgoing <- data
+	}
+}
+
+func (server *Server) Join(connection net.Conn) {
+	client := NewClient(connection, &server.clients)
+	server.clients[connection.RemoteAddr().String()] = client
+	go func() {
+		for {
+			server.incoming <- <-client.incoming
+		}
+	}()
+}
+
+func (server *Server) Listen() {
+	go func() {
+		for {
+			select {
+			case data := <-server.incoming:
+				server.Broadcast(data)
+			case conn := <-server.joins:
+				server.Join(conn)
+			}
+		}
+	}()
+}
+
+func NewServer() *Server {
+	server := &Server{
+		clients:  make(map[string]*Client),
+		joins:    make(chan net.Conn),
+		incoming: make(chan []byte, CONN_MAX_SIZE),
+		outgoing: make(chan []byte, CONN_MAX_SIZE),
+	}
+	server.Listen()
+	return server
+}
+
+func main() {
+	server := NewServer()
+	l, err := net.Listen(CONN_TYPE, CONN_HOST + ":" + CONN_PORT)
     if err != nil {
         fmt.Printf("Error listening on %s:%s: %s", CONN_HOST, CONN_PORT,
 			err.Error())
         os.Exit(1)
     }
 
-    defer l.Close()
+	defer l.Close()
 
     fmt.Println("Listening on " + CONN_HOST + ":" + CONN_PORT)
-    for {
-		// Accept connection
-        conn, err := l.Accept()
+	for {
+		conn, err := l.Accept()
         if err != nil {
             fmt.Println("Error accepting connection: ", err.Error())
             os.Exit(1)
         }
-		// Launch goroutine to service connection
-        go handleRequest(conn)
-    }
+		server.joins <-conn
+	}
 }
 
 func handleRequest(conn net.Conn) {

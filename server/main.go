bb6da1fc8e10844dc89f459f8d1ef91fefccbac0
Eric Park <jeztek@users.noreply.github.com>
2015-02-27 13:00:41 -0800

    working server with local echo suppression

--- server/main.go
+++ server/main.go
@@ -8,6 +8,7 @@ import (
     "os"
 )
 
+// Server parameters
 const (
     CONN_HOST = "localhost"
     CONN_PORT = "8001"
@@ -16,104 +17,16 @@ const (
 	CONN_MAX_SIZE = 8191
 )
 
-type Client struct {
-	connection *net.Conn
-	incoming chan []byte
-	outgoing chan []byte
-	reader   *bufio.Reader
-	writer   *bufio.Writer
-	clients  *map[string]*Client
-}
-
-func (client *Client) Read() {
-	data := make([]byte, CONN_MAX_SIZE)
-	for {
-		n, err := client.reader.Read(data)
-		if (err != nil) {
-			if (err == io.EOF) {
-				fmt.Printf("Client disconnected\n")
-			}
-			fmt.Printf("Error reading from client\n")
-			continue
-		}
-		client.incoming <- data[:n]
-	}
-}
-
-func (client *Client) Write() {
-	for data := range client.outgoing {
-		_, err := client.writer.Write(data)
-		client.writer.Flush()
-		if (err != nil) {
-			fmt.Printf("Error writing to client\n")
-			continue
-		}
-	}
-}
-
-func (client *Client) Listen() {
-	go client.Read()
-	go client.Write()
-}
-
-func NewClient(connection net.Conn, clients *map[string]*Client) *Client {
-	client := &Client{
-		connection: &connection,
-		incoming: make(chan []byte, CONN_MAX_SIZE),
-		outgoing: make(chan []byte, CONN_MAX_SIZE),
-		reader:   bufio.NewReader(connection),
-		writer:   bufio.NewWriter(connection),
-		clients:  clients,
-	}
-	client.Listen()
-	return client
-}
-
-type Server struct {
-	clients  map[string]*Client
-	joins    chan net.Conn
-	incoming chan []byte
-	outgoing chan []byte
-}
-
-func (server *Server) Broadcast(data []byte) {
-	for _, client := range server.clients {
-		client.outgoing <- data
-	}
-}
-
-func (server *Server) Join(connection net.Conn) {
-	client := NewClient(connection, &server.clients)
-	server.clients[connection.RemoteAddr().String()] = client
-	go func() {
-		for {
-			server.incoming <- <-client.incoming
-		}
-	}()
-}
-
-func (server *Server) Listen() {
-	go func() {
-		for {
-			select {
-			case data := <-server.incoming:
-				server.Broadcast(data)
-			case conn := <-server.joins:
-				server.Join(conn)
-			}
-		}
-	}()
-}
-
-func NewServer() *Server {
-	server := &Server{
-		clients:  make(map[string]*Client),
-		joins:    make(chan net.Conn),
-		incoming: make(chan []byte, CONN_MAX_SIZE),
-		outgoing: make(chan []byte, CONN_MAX_SIZE),
-	}
-	server.Listen()
-	return server
+// Control message
+type MsgType int
+const (
+	MsgDisconnect MsgType = iota
+	MsgData       MsgType = iota
+)
+type Message struct {
+	msgtype MsgType
+	sender  string
+	data    []byte
 }
 
 func main() {
@@ -138,15 +51,132 @@ func main() {
 	}
 }
 
-func handleRequest(conn net.Conn) {
-	buf := make([]byte, CONN_MAX_SIZE)
+type Server struct {
+	clients  map[string]*Client
+	joins    chan net.Conn
+	control  chan Message
+	incoming chan Message
+	outgoing chan Message
+}
 
-	readLen, err := conn.Read(buf)
-	if err != nil {
-		fmt.Println("Error reading from client:", err.Error())
+func (server *Server) PrintClients() {
+	fmt.Println("Clients:")
+	for k := range server.clients {
+		fmt.Println(k)
 	}
-	fmt.Printf("Got %d bytes from client\n", readLen);
-	fmt.Printf("rx: % X\n", buf[:readLen])
-	//conn.Write([]byte("Message received"))
-	conn.Close()
+}
+
+func (server *Server) Broadcast(msg Message) {
+	for _, client := range server.clients {
+		if msg.sender != client.id {
+			client.outgoing <- msg
+		}
+	}
+}
+
+func (server *Server) Join(connection net.Conn) {
+	client := NewClient(connection, server)
+	server.clients[connection.RemoteAddr().String()] = client
+	server.PrintClients()
+	go func() {
+		for {
+			server.incoming <- <-client.incoming
+		}
+	}()
+}
+
+func (server *Server) Control(msg Message) {
+	if msg.msgtype == MsgDisconnect {
+		delete(server.clients, msg.sender)
+		server.PrintClients()
+	}
+}
+
+func (server *Server) Listen() {
+	go func() {
+		for {
+			select {
+			case data := <-server.incoming:
+				server.Broadcast(data)
+			case conn := <-server.joins:
+				server.Join(conn)
+			case msg := <-server.control:
+				server.Control(msg)
+			}
+		}
+	}()
+}
+
+func NewServer() *Server {
+	server := &Server{
+		clients:  make(map[string]*Client),
+		joins:    make(chan net.Conn),
+		control:  make(chan Message),
+		incoming: make(chan Message),
+		outgoing: make(chan Message),
+	}
+	server.Listen()
+	return server
+}
+
+type Client struct {
+	id         string
+	connection *net.Conn
+	incoming   chan Message
+	outgoing   chan Message
+	reader     *bufio.Reader
+	writer     *bufio.Writer
+	server     *Server
+}
+
+func (client *Client) Read() {
+	data := make([]byte, CONN_MAX_SIZE)
+	for {
+		n, err := client.reader.Read(data)
+		if (err == io.EOF) {
+			fmt.Printf("Client disconnected\n")
+			break
+		} else if (err != nil) {
+			fmt.Printf("Error reading from client, disconnecting...\n")
+			break
+		} else {
+			client.incoming <- Message{MsgData, client.id, data[:n]}
+		}
+	}
+
+	(*client.connection).Close()
+
+	// Notify server of disconnect
+	client.server.control <- Message{MsgDisconnect, client.id, []byte{}}
+}
+
+func (client *Client) Write() {
+	for msg := range client.outgoing {
+		_, err := client.writer.Write(msg.data)
+		client.writer.Flush()
+		if (err != nil) {
+			fmt.Printf("Error writing to client\n")
+			continue
+		}
+	}
+	fmt.Printf("%s Write() exiting\n", client.id)
+}
+
+func (client *Client) Listen() {
+	go client.Read()
+	go client.Write()
+}
+
+func NewClient(connection net.Conn, server *Server) *Client {
+	client := &Client{
+		id:         connection.RemoteAddr().String(),
+		connection: &connection,
+		incoming:   make(chan Message),
+		outgoing:   make(chan Message),
+		reader:     bufio.NewReader(connection),
+		writer:     bufio.NewWriter(connection),
+		server:     server,
+	}
+	client.Listen()
+	return client
 }
